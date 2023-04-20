9ae613c7c8ee4aed7a0b3940d6ec866bcd134ec1
JesusEstrada201 <127432430+JesusEstrada201@users.noreply.github.com>
2023-04-20 11:28:54 -0600

    actualizar "agregar nueva convocatoria"

--- convocatoria.php
+++ convocatoria.php
@@ -25,12 +25,24 @@
 			<label for="empresa">Empresa:</label>
 			<input type="text" id="empresa" name="empresa"><br>
 			
-			<label for="puesto">Puesto:</label>
+			<label for="puesto">Departamento:</label>
 			<input type="text" id="puesto" name="puesto"><br>
 			
-			<label for="descripcion">Descripción:</label><br>
+			<label for="descripcion">Descripción de Actividades:</label><br>
 			<textarea id="descripcion" name="descripcion" rows="4" cols="50"></textarea><br>
-			
+
+			<label for="horario">Horario:</label>
+			<input type="text" id="horario" name="horario"><br>
+
+			<label for="email">Email:</label>
+			<input type="text" id="email" name="email"><br>
+
+			<label for="colonia">Colonia:</label>
+			<input type="text" id="colonia" name="colonia"><br>
+
+			<label for="calle">Calle:</label>
+			<input type="text" id="calle" name="calle"><br>
+
 			<label for="fecha_inicio">Fecha de inicio:</label>
 			<input type="date" id="fecha_inicio" name="fecha_inicio"><br>
 			
