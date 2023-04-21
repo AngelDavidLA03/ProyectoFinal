306e736630717c7db5979bb58a854e2088329996
JesusEstrada201 <127432430+JesusEstrada201@users.noreply.github.com>
2023-04-20 22:05:09 -0600

    a

--- nuevosvacantes.php
+++ nuevosvacantes.php
@@ -10,7 +10,7 @@
 			<h1>Aceptar Vacantes</h1>
 			<nav>
 				<ul>
-					<li><a href="./index.php">Inicio</a></li>
+					<li><a href="./index.html">Inicio</a></li>
 					<li><a href="./Alumnosenservicio.php">Alumnos</a></li>
 					<li><a href="./convocatoria.php">Convocatorias</a></li>
 					<li><a href="./nuevaconvocatoria.php">Nueva Convocatoria</a></li>
