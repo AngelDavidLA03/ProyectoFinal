9f2db310dbae2a149e96c6d824481733569ba720
Pablo9GM <127432096+Pablo9GM@users.noreply.github.com>
2023-04-21 12:34:38 -0600

    is

--- convocatoria.php
+++ convocatoria.php
@@ -1,4 +1,10 @@
-<div class="container">
-  <h1>En Construccion</h1>
- 
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>En construccion</title>
+</head>
+<body>
+
+	<img src="si.jpg" alt="Descripción de la imagen">
+</body>
+</html>
