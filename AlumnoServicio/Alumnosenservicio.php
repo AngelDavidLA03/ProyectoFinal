791dd884edbfa7ad2b89546d6257d320779e9837
JesusEstrada201 <127432430+JesusEstrada201@users.noreply.github.com>
2023-04-23 22:33:55 -0600

    a

---
+++ AlumnoServicio/Alumnosenservicio.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Alumnos en Servicio Social</title>
+	<link rel="stylesheet" type="text/css" href="stylealumno.css">
+</head>
+
+<header>
+	<div class="container">
+		
+		<nav class="main-menu">
+			<ul>
+				<li><a href="../accounts.html">Cuenta</a></li>
+				<li><a href="./Alumnosenservicio.php">Alumnos en Servicio</a></li>
+				<li><a href="">Nueva Convocatoria</a></li>
+				<li><a href="">Aceptar Aspirantes</a></li>
+        <li><a href="./accounts.html">Cerrar Sesión</a></li>
+
+			</ul>
+		</nav>
+	</div>
+</header>
+
+
+<body>
+
+	<section id="main">
+		<div class="container">
+			<h2>Alumnos en Servicio</h2>
+			<table>
+				<tr>
+					<th>Nombre</th>
+					<th>Especialidad</th>
+                    <th>Departamento</th>
+                    <th>Jefe/Responsable</th>
+					<th>Horas Completadas</th>
+					<th>Apoyo/Beca</th>
+					<th>Documentos</th>
+				</tr>
+				<tr>
+					<td>Pablo Rocha</td>
+					<td>Lic Contabilidad</td>
+                    <td>Contabilidad</td>
+                    <td>Juan Pérez</td>
+                    <th>180</th>
+					<th>Si</th>
+					<td><a href="#">Documentos</a></td>
+				</tr>
+				<tr>
+					<td>Ángel López</td>
+					<td>Lic Administración</td>
+                    <td>Recursos Humanos</td>
+                    <td>Karla Loera</td>
+                    <th>360</th>
+					<th>No</th>
+					<td><a href="#">Documentos</a></td>
+				</tr>
+				<tr>
+					<td>Jesús Estrada</td>
+					<td>Ing Industrial</td>
+                    <td>Calidad</td>
+                    <td>Ana Herrera</td>
+                    <th>300</th>
+					<th>No</th>
+					<td><a href="#">Documentos</a></td>
+				</tr>
+			</table>
+		</div>
+	</section>
+
+	<footer>
+		<div class="container">
+			<p>Derechos Reservados &copy; 2023</p>
+		</div>
+	</footer>
+</body>
+</html>
+
