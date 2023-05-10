7ef6b26fff649f5f466799f8a7397d6a255a4ebf
JesusEstrada201 <127432430+JesusEstrada201@users.noreply.github.com>
2023-05-09 21:10:08 -0600

    a

--- dependencia/cuenta/services.php
+++ dependencia/cuenta/services.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cuenta</title>
   
-    <link rel="stylesheet" type="text/css" href="stylegaleria.css">
+    <link rel="stylesheet" type="text/css" href="./stylegaleria.css">
 
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
