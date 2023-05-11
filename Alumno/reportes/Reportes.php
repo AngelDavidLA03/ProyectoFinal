7ade82923efe38e693067e2ca6c6c4e16f2a024a
Pablo9GM <127432096+Pablo9GM@users.noreply.github.com>
2023-05-10 23:44:52 -0600

    update

--- Alumno/reportes/Reportes.php
+++ Alumno/reportes/Reportes.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" type="text/css" href="stylereporte.css">
-</head>
-</head>
-<body>
+  <title>Reporte de Servicio Social</title>
+  <link rel="stylesheet" href="stylereporte.css">
+
+  <body>
   <div class="header">
-  <a href="../estudiante.php" class="Inicio">Inicio</a>
     <h2 class="centered-heading">PORTAL</h2>
-    
     <div class="user-menu">
       <a href="#" class="user-icon"><img src="https://via.placeholder.com/150" alt="User"></a>
       <ul class="dropdown-menu">
@@ -18,17 +16,42 @@
       </ul>
     </div>
   </div>
-<body>
-  <div class="center">
+</div>
+</head>
+<body> <div class="container">
+    <h1>Reporte de Servicio Social</h1>
+    <a class="download-btn" href="ruta_del_archivo" download>Descargar documento aquí</a>
+    
     <div class="team-block">
-      <h2>Descargar archivo</h2>
-      <a href="Reporte Servicio Social.doc" download class="button">Descargar</a>
-    </div>
-    <div class="team-block">
-      <h2>Subir archivo</h2>
-      <input type="file" class="file-input">
-      <button class="button">Subir</button>
+      <label for="fileInput">Seleccionar archivo:</label>
+      <label class="custom-file-input" for="fileInput">Explorar archivos</label>
+      <input type="file" id="fileInput">
+      <span class="file-name"></span>
+      <button onclick="uploadFile()">Subir</button>
     </div>
   </div>
+
+  <script>
+    var fileInput = document.getElementById('fileInput');
+    var fileNameContainer = document.querySelector('.file-name');
+    
+    fileInput.addEventListener('change', function() {
+      fileNameContainer.textContent = this.files[0].name;
+    });
+    
+    function uploadFile() {
+      var file = fileInput.files[0];
+      // Aquí puedes realizar las acciones necesarias para subir el archivo
+    }
+  </script>
+</body>
+</html>
+  <script>
+    function uploadFile() {
+      var fileInput = document.getElementById('fileInput');
+      var file = fileInput.files[0];
+      // Aquí puedes realizar las acciones necesarias para subir el archivo
+    }
+  </script>
 </body>
 </html>
