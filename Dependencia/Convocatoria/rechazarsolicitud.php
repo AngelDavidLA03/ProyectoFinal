365e9fa406006c6b1d4b994e933f1dfbf03c9cdf
Angel David López Álvarez <127431937+AngelDavidLA03@users.noreply.github.com>
2023-05-17 02:00:44 -0600

    haber si no se apendeja

--- Dependencia/Convocatoria/rechazarsolicitud.php
+++ Dependencia/Convocatoria/rechazarsolicitud.php
@@ -1,12 +1,9 @@
 <?php
 session_start(); // Iniciar la sesión
 
-// Obtener el archivo subido
-$file = $_FILES['file'];
-
 // Obtener los datos necesarios para los procedimientos almacenados
-$matricula = $_SESSION["user"];
-$tipoDocument = "PDF";
+$alumno = (string)$_POST['alumn'];
+$id = (string)$_POST['servc'];
 
 // Llamar a los procedimientos almacenados mediante una conexión a la base de datos
 $mysqli = new mysqli('localhost', 'root', 'AdLa20031108', 'db_serviciosocial'); // Reemplaza con la configuración correcta de la base de datos
@@ -18,20 +15,11 @@ if ($mysqli->connect_errno) {
 }
 
 // Llamar al procedimiento SAVEdocuments
-$stmt2 = $mysqli->prepare("CALL SAVEdocuments(?, ?)");
-$stmt2->bind_param("sb", $file['tmp_name'], $tipoDocument);
-$stmt2->send_long_data(0, file_get_contents($file['tmp_name']));
+$stmt2 = $mysqli->prepare("CALL rejectAlumn(?, ?)");
+$stmt2->bind_param("ss",$alumno,$id);
+
 $stmt2->execute();
 $stmt2->close();
-
-// Obtener el ID del documento insertado
-$idDocument = $mysqli->insert_id;
-
-// Llamar al procedimiento SAVErac
-$stmt1 = $mysqli->prepare("CALL SAVErac(?, ?)");
-$stmt1->bind_param("si", $idDocument, $matricula);
-$stmt1->execute();
-$stmt1->close();
 
 $mysqli->close();
 ?>
